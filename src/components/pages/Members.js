14b85070e7efccd76f74a71d6d4899f6a3900180
SipanAkcan <67048219+SipanAkcan@users.noreply.github.com>
2022-12-12 01:20:24 +0300

    fixx

--- src/components/pages/Members.js
+++ src/components/pages/Members.js
@@ -12,8 +12,6 @@ function members(){
 //    const linkedin = require("../../assets/linkedin.png");
 const sipan_merhas = require("../../assets/fusion-member-photo/sipan_merhas.JPG");
 const mertali_ipek = require("../../assets/fusion-member-photo/mertali_ipek.JPG");
-const nisanur_gülerce = require("../../assets/fusion-member-photo/nisanur_gülerce.JPG");
-
 const rana_fidan = require("../../assets/fusion-member-photo/rana_fidan.JPG");
 const sude_güçer = require("../../assets/fusion-member-photo/sude_güçer.JPG");
 const şevval_arslantürk = require("../../assets/fusion-member-photo/şevval_arslantürk.JPG");
@@ -403,7 +401,7 @@ const bayram_kuru = require("../../assets/fusion-member-photo/bayram_kuru.png");
 >
                 <Card  border="light" style={{
                     width:270,}}>
-                    <Image variant="top" src={nisanur_gülerce}
+                    <Image variant="top" src={""}
                             width={"%100"}
                             height={"%100"}  />
                     <Card.Body>
